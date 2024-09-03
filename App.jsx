19776dc51348208522d6f0f4d182dd14c1faf897
abdan <adliyanabdan@gmail.com>
2024-09-03 12:55:26 +0700

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Board from './Board';
+import './App.css';
+
+function App() {
+  return (
+    <div className="game">
+      <h1>Tic-Tac-Toe</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
